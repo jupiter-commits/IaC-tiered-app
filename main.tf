cec8b58fc164d20b8fd4a261f91f8abf6639b401
jupiter-commits <192867955+jupiter-commits@users.noreply.github.com>
2025-03-29 19:40:16 +0200

    Add subnets

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@
  }
 
  provider "aws" {
-  region = "us-east-1" #$
+  region = "us-east-1"
  }
 
 
@@ -20,9 +20,42 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
-resource "aws_subnet" "aws_subnet_public" {
+resource "aws_subnet" "public_1" {
   cidr_block = "10.0.0.0/24"
   vpc_id = aws_vpc.main_vpc.id
-  tags = {Name = "Public subnet"}
+  tags = {Name = "Public subnet 1"}
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "Internet Gateway"
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Private subnet 1"
+  }
+}
+
+resource "aws_eip" "eip_ngw" {
+  vpc = true
+  depends_on = [aws_internet_gateway.igw_1]
+  tags = {
+    Name = "ELastic IP for NAT Gateway"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.eip_ngw_1
+  subnet_id = aws_subnet.aws_subnet_public
+
+  tags = {
+    Name = "NAT Gateway"
+  }
+}

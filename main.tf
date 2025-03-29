71c75af559160c2f774374a0d72109a80575a179
jupiter-commits <192867955+jupiter-commits@users.noreply.github.com>
2025-03-29 19:45:55 +0200

    Add route tables

--- main.tf
+++ main.tf
@@ -45,17 +45,55 @@ resource "aws_subnet" "private_1" {
 
 resource "aws_eip" "eip_ngw" {
   vpc = true
-  depends_on = [aws_internet_gateway.igw_1]
+  depends_on = [aws_internet_gateway.igw]
   tags = {
     Name = "ELastic IP for NAT Gateway"
   }
 }
 
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.eip_ngw_1
+  allocation_id = aws_eip.eip_ngw
   subnet_id = aws_subnet.aws_subnet_public
 
   tags = {
     Name = "NAT Gateway"
   }
 }
+
+resource "aws_route_table" "route_public_subnet" {
+  vpc_id = aws_vpc.main_vpc
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Route table for public subnet"
+  }
+}
+
+resource "aws_route_table_association" "route_public_association" {
+  subnet_id = aws_subnet.public_1.id
+  route_table_id = aws_route_table.route_public_subnet.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main_vpc
+
+  route = {
+    cidr_block = "0.0.0.0/0" # (
+    gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "Route table for private subnet"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+

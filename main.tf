5721f61976a6aba20ef373dd9f69d338b50c36e8
jupiter-commits <192867955+jupiter-commits@users.noreply.github.com>
2025-04-17 21:32:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
 
-module "vpc_example_simple" {
-  source  = "terraform-aws-modules/vpc/aws//examples/simple"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
   version = "5.19.0"
 
   name = "main_environment"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.0.0/24"]
-  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.1.0/24", "10.0.12.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+
+  enable_nat_gateway = false
 
   tags = {
     Environment = "main"

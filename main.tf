37db2b1505c856db12cc53f803750e3692ddf2f6
jupiter-commits <192867955+jupiter-commits@users.noreply.github.com>
2025-04-25 17:15:29 +0300

    Change region

--- main.tf
+++ main.tf
@@ -6,11 +6,12 @@ module "vpc" {
   name = "main_environment"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
+  azs             = ["eu-west-3a", "eu-west-3b"]
   public_subnets  = ["10.0.1.0/24", "10.0.12.0/24"]
   private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
 
-  enable_nat_gateway = false
+  create_database_subnet_group = false
+  enable_nat_gateway           = false
 
   tags = {
     Environment = "main"

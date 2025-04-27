d77592db2fa2584681afe2a0473fd59a7d94be75
jupiter-commits <192867955+jupiter-commits@users.noreply.github.com>
2025-04-27 23:16:50 +0300

    Update variables

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
-    type = string
-    default = "eu-west-3"  
+  type    = string
+  default = "eu-west-3"
 }
 
 variable "instance_type" {
-    type = string
-    default = "t2.micro"
-    description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+  description = "EC2 instance type"
 }

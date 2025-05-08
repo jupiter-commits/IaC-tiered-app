608fec2809b25acbc3bf7b9bb8da41fe2ed7ea63
jupiter-commits <192867955+jupiter-commits@users.noreply.github.com>
2025-05-08 22:58:03 +0300

    Add ingress rules

--- main.tf
+++ main.tf
@@ -33,6 +33,22 @@ resource "aws_security_group" "public_subnet" {
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.public_subnet.id
+  cidr_ipv4         = module.vpc.default_vpc_cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.public_subnet.id
+  cidr_ipv4         = module.vpc.default_vpc_cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.public_subnet.id
   cidr_ipv4         = "0.0.0.0/0"
